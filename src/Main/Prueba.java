e58c73c9ddb710b114518a3b298b3e148ac20746
MrJoelV <66540690+MrJoelV@users.noreply.github.com>
2021-06-08 16:19:23 -0500

    0,1 commit joel 2

--- src/Main/Prueba.java
+++ src/Main/Prueba.java
@@ -13,7 +13,7 @@ public class Prueba {
     
     public static void main(String[] args){
         
-        System.out.println("Hola soys Joel");
+        System.out.println("Hola soys Joeladasdas");
     
     }
     
