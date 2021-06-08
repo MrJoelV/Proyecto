5a185833e98e3a9aaa2a348c3314db7fc5e5cee6
MrJoelV <66540690+MrJoelV@users.noreply.github.com>
2021-06-08 16:42:59 -0500

    0.1 Joel 12

--- src/Main/Prueba.java
+++ src/Main/Prueba.java
@@ -13,7 +13,7 @@ public class Prueba {
     
     public static void main(String[] args){
         
-        System.out.println("Hola");
+        System.out.println("Hola asdasdas");
     
     }
     
