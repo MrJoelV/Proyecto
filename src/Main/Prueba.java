13c1557447a5a2cc3830d10f8057cc251bbc9d49
raull <raull@DESKTOP-2TMK9MT>
2021-06-08 16:29:52 -0500

    cambiar hola por Hola Soy Ral

--- src/Main/Prueba.java
+++ src/Main/Prueba.java
@@ -13,7 +13,8 @@ public class Prueba {
     
     public static void main(String[] args){
         
-        System.out.println("Hola");
+        System.out.println("Hola Soy Raul");
+        
     
     }
     
